d788350063b89b618d73f64eaf69689f7e355779
Roshan Choxi <roshan.choxi@gmail.com>
2017-06-28 18:10:25 -0500

    show recent components

--- src/components/new_dialog.jsx
+++ src/components/new_dialog.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { ipcRenderer } from 'electron'
+import fs from 'fs'
+import path from 'path'
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+}
 
 export default class NewDialog extends React.Component {
   createComponent() {
     let name = this.nameInput.value
 
-    ipcRenderer.send("open-component", { 
-      name: name
-    })
-
+    ipcRenderer.send("open-component", { name: name })
     this.nameInput.value = ""
   }
 
+  openComponent(name) {
+    ipcRenderer.send("open-component", { name: name })
+  }
+
   render() {
-    let components = JSON.parse(localStorage.components || "[]")
-    let componentsPartial = components.map((component) => {
-      return <li>{ component.name }</li>
+    let components = getDirectories("./user") || JSON.parse(localStorage.components || "[]")
+    let componentsPartial = components.map((name) => {
+      return <li onClick={ () => this.openComponent(name) } >{ name }</li>
     })
 
     return <div className="NewDialog">
