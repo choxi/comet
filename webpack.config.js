d7672f8146c9ef9b24d4ed04157093ca2f569d71
Roshan Choxi <roshan.choxi@gmail.com>
2017-07-06 13:28:17 -0500

    bundle css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var glob = require("glob")
-var path = require("path")
+const path              = require("path")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+let extractTextPlugin   = ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) 
 
 module.exports = {
   entry: {
@@ -14,13 +16,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(jsx|js)$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['env', 'react']
           }
         }
+      },
+      {
+         test: /\.css$/,
+         use: extractTextPlugin
       }
     ]
-  }
+  },
+  plugins: [ new ExtractTextPlugin({ filename: "[name].css" }) ]
 }
